d94d924293810151a63fe397c571144e62b4e996
fabricius1 <fabriciusbr@gmail.com>
2021-03-22 10:59:22 -0300

    Adaptation to use in posix OS

--- duckytie.py
+++ duckytie.py
@@ -3,12 +3,23 @@ from gtts import gTTS
 import time
 import os
 
+if os.name == "posix":
+    from pydub import AudioSegment
+
 
 # PART 1
-def say(text, filename="temp.mp3", delete_mp3=True, language="en", slow=False):
+def say(text, filename="temp.mp3", delete_audio_file=True, language="en", slow=False):
     # PART 2
     audio = gTTS(text, lang=language, slow=slow)
     audio.save(filename)
+
+    if os.name == "posix":
+        sound = AudioSegment.from_mp3(filename)
+        old_filename = filename
+        filename = filename.split(".")[0] + ".ogg"
+        sound.export(filename, format="ogg")
+        if delete_audio_file:
+            os.remove(old_filename)
 
     # PART 3
     mixer.init()
@@ -23,10 +34,12 @@ def say(text, filename="temp.mp3", delete_mp3=True, language="en", slow=False):
 
     # PART 5
     mixer.quit()
-    if delete_mp3:
+    if delete_audio_file:
         os.remove(filename)
-    print(f"mp3 file played for {seconds} seconds")
+    print(f"audio file played for {seconds} seconds")
 
 
 if __name__ == "__main__":
+    say("You are worthy of love!")
+    time.sleep(1.5)
     say("This is the main module. This program has finished. Thanks for running it!")
