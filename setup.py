8b95ca44886e52c25f16b055005611d5ed19e913
fabricius1 <fabriciusbr@gmail.com>
2021-03-29 20:42:25 -0300

    setup.py modified

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     name="duckytie",
     packages=["duckytie"],
     version="0.1",
-    license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    license="MIT",
     description="Make your program read texts aloud with our say() function.",
-    author="Fabricio Brasil",                   # Type in your name
-    author_email="fabriciusbr@gmail.com",      # Type in your E-Mail
+    author="Fabricio Brasil",
+    author_email="fabriciusbr@gmail.com",
     url="https://github.com/fabricius1/duckytie",
-    #   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+    download_url="https://github.com/fabricius1/duckytie/archive/refs/tags/v_01.tar.gz",
     keywords=["text", "audio", "converter", "say", "function",
               "duckytie", "module"],
     install_requires=[
@@ -19,13 +19,10 @@ setup(
         "pydub",
     ],
     classifiers=[
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         "Development Status :: 3 - Alpha",
-        # Define that your audience are developers
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: MIT License",   # Again, pick a license
-        # Specify which pyhton versions that you want to support
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
